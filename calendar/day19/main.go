954720e3bc9c5d7e5f05d038662bff16e9945e28
JRaams <47593569+JRaams@users.noreply.github.com>
2020-12-19 17:17:53 +0100

    Solution day 19 part b

--- calendar/day19/main.go
+++ calendar/day19/main.go
@@ -15,11 +15,14 @@ func main() {
 	inputPath, _ := filepath.Abs("input")
 	lines := helpers.GetInputValues(inputPath)
 	rules, messages := load(lines)
-	regex := getRegex(rules, 0)
 
 	// Part 1
-	a := solveA(regex, messages)
+	a := solve(rules, messages, false)
 	fmt.Printf("Solution day 19 part a: %d\n", a)
+
+	// Part 2
+	b := solve(rules, messages, true)
+	fmt.Printf("Solution day 19 part b: %d\n", b)
 }
 
 type rule struct {
@@ -74,52 +77,71 @@ func load(lines []string) (map[int]rule, []string) {
 	return rules, messages
 }
 
-func getRegex(rules map[int]rule, rootRuleNum int) string {
+func getRegex(rules map[int]rule, rootRuleNum int, useFixedRules bool) string {
 	r := ""
 	rootRule := rules[rootRuleNum]
+
+	// Fix rules 8 & 11
+	if useFixedRules {
+		if rootRuleNum == 8 {
+			return getRegex(rules, 42, useFixedRules) + "+"
+		}
+		if rootRuleNum == 11 {
+			return getRegex(rules, 42, useFixedRules) + "{n}" + getRegex(rules, 31, useFixedRules) + "{n}"
+		}
+	}
 
 	// Reached a char rule, return it
 	if rootRule.char != "" {
 		return rootRule.char
 	}
 
-	//  Rule contains multiple subrules
+	//  Rule contains two subrules
 	if len(rootRule.subRuleNumLists) == 2 {
 		r += "("
 		for _, subRuleNum := range rootRule.subRuleNumLists[0] {
-			r += getRegex(rules, subRuleNum)
+			r += getRegex(rules, subRuleNum, useFixedRules)
 		}
 		r += "|"
 		for _, subRuleNum := range rootRule.subRuleNumLists[1] {
-			r += getRegex(rules, subRuleNum)
+			r += getRegex(rules, subRuleNum, useFixedRules)
 		}
 		r += ")"
 		return r
 	}
 
+	// Rule is a single list of subrules
 	if len(rootRule.subRuleNumLists) == 1 {
 		for _, subRuleNum := range rootRule.subRuleNumLists[0] {
-			r += getRegex(rules, subRuleNum)
+			r += getRegex(rules, subRuleNum, useFixedRules)
 		}
 	}
 
 	return r
 }
 
-func solveA(regex string, messages []string) int {
-	a := 0
+func solve(rules map[int]rule, messages []string, useFixedRules bool) int {
+	regex := getRegex(rules, 0, useFixedRules)
 	regex = "^" + regex + "$"
+	matchedMessageCount := 0
 
-	r, err := regexp.Compile(regex)
-	if err != nil {
-		panic(err)
+	maxN := 10
+	if useFixedRules {
+		maxN = 1
 	}
 
 	for _, message := range messages {
-		matches := r.Match([]byte(message))
-		if matches {
-			a++
+		for n := 1; n <= maxN; n++ {
+			quantificationAmount := strings.ReplaceAll(regex, "n", strconv.Itoa(n))
+			r, _ := regexp.Compile(quantificationAmount)
+
+			matches := r.Match([]byte(message))
+			if matches {
+				matchedMessageCount++
+				break
+			}
 		}
 	}
-	return a
+
+	return matchedMessageCount
 }
---
+++ calendar/day19/solution.md
@@ -0,0 +1,5 @@
+```bash
+$ go run .
+Solution day 19 part a: 118
+Solution day 19 part b: 246
+```
